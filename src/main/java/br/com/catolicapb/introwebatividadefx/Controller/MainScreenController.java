4c2884e28642a5961428d1e992d86df396a1fb0d
Jefferson <jefferson.sg91@hotmail.com>
2024-10-30 17:37:24 -0300

    dawdawda

--- src/main/java/br/com/catolicapb/introwebatividadefx/Controller/MainScreenController.java
+++ src/main/java/br/com/catolicapb/introwebatividadefx/Controller/MainScreenController.java
@@ -1,13 +1,9 @@
 package br.com.catolicapb.introwebatividadefx.Controller;
 
-import br.com.catolicapb.introwebatividadefx.Dao.UserDao;
 import br.com.catolicapb.introwebatividadefx.Interface.IOnChangeScreen;
-import br.com.catolicapb.introwebatividadefx.Model.User;
-import br.com.catolicapb.introwebatividadefx.Service.AuthService;
 import br.com.catolicapb.introwebatividadefx.Util.ScreenManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 
 
 public class MainScreenController implements IOnChangeScreen {
@@ -40,17 +36,5 @@ public class MainScreenController implements IOnChangeScreen {
     @FXML
     void btUserManagementAction() {
 
-    }
-
-    private void teste() {
-        String token = AuthService.getAccessToken();
-        UserDao userDao = new UserDao();
-        User loggedInUser = userDao.getUserById(token);
-
-        if (loggedInUser != null) {
-            System.out.println("Usuário logado: " + loggedInUser.getUsername());
-        } else {
-            System.out.println("null");
-        }
     }
 }
